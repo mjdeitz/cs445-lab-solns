6eb3a11b46eed09e465dd43dfe303523ce60401c
mjdeitz <mjdeitz@yahoo.com>
2021-11-01 19:49:07 -0500

    lab4 solns

---
+++ Lab4/Exercise2/calculator.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    document.getElementById('submit').onclick = function () {
+        let num1 = document.getElementById('num1').value;
+        let num2 = document.getElementById('num2').value;
+        document.getElementById('result').value = parseInt(num1) + parseInt(num2);
+
+        //some companies want code to be precise - see below
+        // document.getElementById('result').value = parseInt(document.getElementById('num1').value)
+        //     + parseInt(document.getElementById('num2').value);
+    }
+}
